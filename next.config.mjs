f59e458b1cb61385d0835d0023095fa404591fed
NyoMinKhat <108140828+nyominkhat@users.noreply.github.com>
2025-03-25 11:26:44 +0630

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,16 +8,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'flapi.tagoplus.co.kr',
-      },
-      {
-        protocol: 'https',
-        hostname: 'flapi.tagoplus.co.kr',
-      },
-      {
-        protocol: 'http',
         hostname: 'localhost',
-        port: '5014',
+        port: '3000',
       },
     ],
   },
